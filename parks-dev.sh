4dbbd890f2bc9a0c93ca4d2e1ce51abffae12b49
LekWut <ukkarapol@gmail.com>
2018-12-28 21:28:32 +0700

    Update parks-dev.sh

--- parks-dev.sh
+++ parks-dev.sh
@@ -2,4 +2,5 @@ GUID=$1
 echo "Setting up Parks Dev in project $GUID-parks-dev"
 oc new-project ${GUID}-parks-dev
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-dev
+oc policy add-role-to-user view --serviceaccount=default -n ${GUID}-parks-dev
 oc new-app -f parks-dev.yaml
